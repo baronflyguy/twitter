8a85a603e7d71422e4f7a435b05710d77decf8bb
unknown <Jonathan@Jonathan-PC.(none)>
2012-05-10 22:48:54 +0100

    changed twitter key and secret

--- server/keys.js
+++ server/keys.js
@@ -2,7 +2,7 @@
 exports.keyid  = 'YOUR_AWS_KEY_ID_hello'
 exports.secret = 'YOUR_AWS_SECRET_KEY'
 
-exports.twitter  = { keyid:'YOUR_TWITTER_KEY_ID', secret:'YOUR_TWITTER_SECRET' }
+exports.twitter  = { keyid:'GeTigWLkgKf9PlT7AFlpHQ', secret:'IGObKLRocWJkT5fjrqCPsFnrcHX6Ehhsm9mFUX17Wc' }
 exports.facebook = { keyid:'YOUR_FACEBOOK_KEY_ID', secret:'YOUR_FACEBOOK_SECRET' }
 exports.linkedin = { keyid:'YOUR_LINKEDIN_KEY_ID', secret:'YOUR_LINKEDIN_SECRET' }
 
